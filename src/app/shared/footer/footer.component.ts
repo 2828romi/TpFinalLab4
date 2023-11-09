874d769332eb3cff63966ea677f8fdcb9bb82192
eli-ana-rjs <elianabellarojas@gmail.com>
2023-11-08 23:08:53 -0300

    Agregar estilos basicos al footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  rominaLink = 'https://www.linkedin.com/in/romina-barragan-841743233';
+  elianaLink = 'https://www.linkedin.com/in/eliana-anabella-rojas';
+  jeremiasLink = 'https://www.linkedin.com/in/jeremias-doumic-7402521ab';
 }
